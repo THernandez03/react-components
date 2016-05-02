51626581b6a1eeb7c36d976da0f314edf301bc13
Tomás Hernández <tomas.hernandez03@gmail.com>
2016-05-02 12:13:57 -0300

    Quitar __BROWSER__ como 'env'

--- private/config/envs.js
+++ private/config/envs.js
@@ -4,7 +4,6 @@ const argv = process.argv || [];
 const prod = 'production';
 const dev = 'development';
 const local = 'local';
-const browser = 'browser';
 
 let env = argv[2] || envs.NODE_ENV || local;
 
@@ -14,10 +13,6 @@ export const __DEV__ = env === dev;
 if(!__PROD__ && !__DEV__){
   env = local;
 }
-if(typeof window === 'object'){
-  env = browser;
-}
 
 export const __LOCAL__ = env === local;
-export const __BROWSER__ = env === browser;
 export const __ENV__ = env;
