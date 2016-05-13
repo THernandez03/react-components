7c3e6bd97d80177cb25c90bb48a7e8f4b923a48a
Tomás Hernández <tomas.hernandez03@gmail.com>
2016-05-13 18:38:30 -0300

    Remover 'items' de simple.jsx

--- private/components/simple.jsx
+++ private/components/simple.jsx
@@ -4,10 +4,9 @@ import Simple from '~/components/dumb/simple';
 import { addItem } from '~/actions/';
 
 export default connect(
-  (state) => {
+  () => {
     return {
-      items: state.items
-    , text: 'This text has been overriden by the `state` value'
+      text: 'This text has been overriden by the `state` value'
     };
   }
 , (dispatch) => {
